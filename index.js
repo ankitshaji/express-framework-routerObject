20382d49e5ee8823df04615b71ee695230a94d27
Ankit Shaji <ankitshaji@gmail.com>
2022-09-04 08:38:55 +1200

    initialize express app

---
+++ index.js
@@ -0,0 +1,10 @@
+//main file of this app that gets other npm package modules or user created modules
+// ********************************************************************************
+//RESTful webApi - using REST principles
+// ********************************************************************************
+const express = require("express"); //functionObject //express module
+const app = express(); //appObject
+
+// *****************************************************************************************
+//RESTful webApi crud operations pattern (route/pattern matching algorithm - order matters)
+// *****************************************************************************************
